3d6693aa94ecc9ee8a2a7a5d693698b3ad3f4e71
msaivasanth <vasanths314@gmail.com>
2023-08-24 20:08:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ChatPage from './Components/ChatPage';
+import Home from './Components/Home';
+import ChatState from './context/ChatState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ChatState>
+        <Router>
+        <ChakraProvider>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/chatpage">
+                <ChatPage />
+              </Route>
+              </Switch>
+        </ChakraProvider>
+        </Router>
+      </ChatState>
     </div>
   );
 }
